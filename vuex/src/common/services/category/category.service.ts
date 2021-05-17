62fb84e9b9e29f3e2b645b74047d1885939a6018
javisolercanto <jsolercanto11@gmail.com>
2021-05-17 17:44:24 +0200

    changes on category service

--- vuex/src/common/services/category/category.service.ts
+++ vuex/src/common/services/category/category.service.ts
@@ -1,28 +1,8 @@
 import Vue from "vue";
-import axios from "axios";
-import VueAxios from "vue-axios";
-import { API_URL } from "../../config";
-import { getToken } from "../../jwt.service";
-import ApiService from "../api.service";
 
 const modelUrl = 'category';
 
 const CategoryService = {
-    init() {
-      Vue.use(VueAxios, axios);
-      Vue.axios.defaults.baseURL = API_URL;
-    },
-  
-    setHeader() {
-      Vue.axios.defaults.headers.common["x-access-token"] = `${getToken()}`;
-    },
-  
-    query(resource, params) {
-      return Vue.axios.get(resource, params).catch(error => {
-        throw new Error(`[RWV] ApiService ${error}`);
-      });
-    },
-
     get(categoryId) {
       return Vue.axios.get(`${modelUrl}/?${categoryId}`).catch(error => {
         throw new Error(`[RWV] Category ${error}`);
@@ -41,10 +21,6 @@ const CategoryService = {
   
     update(categoryId, category) {
       return Vue.axios.put(`${modelUrl}/${categoryId}/`, category);
-    },
-  
-    edit(categoryId, category) {
-      return Vue.axios.put(`${modelUrl}/${categoryId}`, category);
     },
   
     delete(categoryId) {
